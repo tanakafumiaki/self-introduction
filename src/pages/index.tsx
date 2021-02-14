0dee90c00c1671d73281f3904644e1c96e3dca18
tanakafumiaki <fumiakishodai@gmail.com>
2021-02-14 17:02:25 +0900

    pages_index_to_arrow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
 import styles from 'src/styles/Home.module.sass';
 import {PrimaryButton} from "src/components/atoms";
+import React from "react";
 
-export default function Home() {
-  return (
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          WELCOME
-        </h1>
+const Home　= () => {
+    return (
+        <main className={styles.main}>
+            <h1 className={styles.title}>
+                WELCOME
+            </h1>
 
-        <PrimaryButton
-            text="Click"
-            link="/selection"/>
-      </main>
-  )
+            <PrimaryButton
+                text="Click"
+                link="/selection"/>
+        </main>
+    )
 }
+export default Home
