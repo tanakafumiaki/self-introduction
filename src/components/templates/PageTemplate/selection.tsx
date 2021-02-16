bf2f4341b359c46ac2375a33e151bc3539fe1b27
tanakafumiaki <fumiakishodai@gmail.com>
2021-02-16 17:37:09 +0900

    create_selection_page

---
+++ src/components/templates/PageTemplate/selection.tsx
@@ -0,0 +1,32 @@
+import styles from "src/components/templates/PageTemplate/styles.module.sass";
+import {PrimaryLink} from "src/components/atoms";
+import React from "react";
+
+const SelectionTemplate: React.VFC　= () => {
+    return (
+        <main className={styles.main}>
+            <h1 className={styles.title}>
+                WELCOME
+            </h1>
+
+            <div className={styles.selectionContainer}>
+            <PrimaryLink
+                text="Profile"
+                href="/profile"/>
+
+            <PrimaryLink
+                text="Skills"
+                href="/skills"/>
+
+            <PrimaryLink
+                text="Products"
+                href="/products"/>
+
+            <PrimaryLink
+                text="Experience"
+                href="/experience"/>
+            </div>
+        </main>
+    )
+}
+export default SelectionTemplate;
---
+++ src/pages/selection.tsx
@@ -0,0 +1,7 @@
+import SelectionTemplate from "src/components/templates/PageTemplate/selection";
+import React from "react";
+
+const Selection: React.VFC = () => {
+    return <SelectionTemplate />
+}
+export default Selection;
