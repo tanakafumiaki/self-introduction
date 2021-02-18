db5dad2216dd50bc6e5458cde619ff0b20bca5de
tanakafumiaki <fumiakishodai@gmail.com>
2021-02-18 14:35:12 +0900

    create_SelectionTemplate_styles.module.sass

--- src/components/templates/SelectionTemplate/index.tsx
+++ src/components/templates/SelectionTemplate/index.tsx
@@ -1,4 +1,4 @@
-import styles from "components/templates/HomeTemplate/styles.module.sass";
+import styles from "components/templates/SelectionTemplate/styles.module.sass";
 import {PrimaryLink} from "components/atoms";
 import React from "react";
 
