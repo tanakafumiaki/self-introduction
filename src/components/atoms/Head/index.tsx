c3eaf3ec6c4ed1269279c296202888ae675c9759
tanakafumiaki <fumiakishodai@gmail.com>
2021-03-12 17:23:33 +0900

    add_mixin_and_responsive_selectionPage

--- src/components/atoms/Head/index.tsx
+++ src/components/atoms/Head/index.tsx
@@ -9,6 +9,7 @@ const Header: React.VFC<Props> = ({ tabName }) => {
     return (
         <Head>
             <title>{tabName}</title>
+            <meta name="viewport" content="width=device-width"/>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
             <link href="https://fonts.googleapis.com/css2?family=Ribeye+Marrow&display=swap" rel="stylesheet"/>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
